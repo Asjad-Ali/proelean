cf75a5c3c5571ebdeb93938bb79be4c089a0a701
AsifAli13280 <asifali@horizam.com>
2022-02-04 19:20:13 +0500

    become seller api implemented

--- src/store/modules/becomeFreelancer.store.js
+++ src/store/modules/becomeFreelancer.store.js
@@ -1,15 +1,34 @@
 import Api from '@/services/API'
+// import { createToaster } from '@meforma/vue-toaster';
 
 
 export const state = {
+    countries: [],
+    languages: [],
+    deliveryDays: [],
     error: null,
   }
 
 export const  mutations = {
-    setError(state,error){
-      state.error=error;
-    }
+  setCountries(state,countries) {
+    state.countries = countries;
+  },
+  setLanguages(state,languages) {
+    state.languages = languages;
+  },
+  setDeliveryDays(state,deliveryDays) {
+    state.deliveryDays = deliveryDays;
+  },
+  setError(state,error){
+    state.error=error;
   }
+}
+
+export const getters = {
+  getCountries: state => state.countries,
+  getLanguages: state => state.languages,
+  getDeliveryDays: state => state.deliveryDays,
+}
 
 export const  actions = {
     getCategories({commit},payload){
@@ -25,7 +44,27 @@ export const  actions = {
       });
     },
 
-    register(state,payload){
+    async getCountriesLanguage({ commit }) {
+      const response = await Api.get('countries&categories');
+      if(response.status === 200) {
+        commit('setCountries',response.data.countries);
+        commit('setLanguages',response.data.languages);
+        commit('setDeliveryDays',response.data.delivery_days);
+      }
+    },
+
+    async handleBecomeSeller(payload) {
+      console.log("payload: "+ JSON.stringify(payload))
+      // const res = await Api.post('become_freelancer',payload)
+      // if(res.status === 200) {
+      //   const toaster = createToaster()
+      //   toaster.success(res.message,{
+      //     position:"top-right",
+      //     dismissible: true});
+      // }
+    },
+
+    register(state,payload) {
         return Api.post('register',payload).then(
           resp=>{
           //   this.commit('LOGIN_SUCCESS');
