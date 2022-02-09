025b34e30bfe61c0aa879a8ae89ad321ad71a86c
Asjad-Ali <92371450+Asjad-Ali@users.noreply.github.com>
2022-02-09 19:45:43 +0500

    Update gig work

---
+++ src/composables/useSeller.js
@@ -0,0 +1,78 @@
+import { ref } from "vue";
+import { useStore } from "vuex";
+
+export default function seller(){
+
+    const store = useStore();
+    const bannersBase64= ref([]);
+    const preview = ref(null);
+
+    const data = ref({
+        description: '',
+        category_id: '',
+        sub_category_id: '',
+    });
+
+
+    const convertFileToBase64 = (file) => {
+        data.value.cinic = file;
+        const reader = new FileReader();
+        reader.onloadend = function() {
+            preview.value = reader.result;
+        }
+        reader.readAsDataURL(file);
+    }
+
+    const createGig = ref({
+        s_description: "",
+        description: "",
+        banner: [],
+        category_id: "d3JkUHdCcEZRMGdseTBvNXFOVXNGUT09",
+        sub_category_id: "cU1VOWVkQVVpVElJdll4eThYOXpBZz09",
+        delivery_time: "20 days",
+        price: "",
+      });
+
+      const  gigCreation = () => {
+        store.dispatch('createGig',createGig.value)
+        console.log("Create Gig Value", createGig.value);
+      }
+      
+    const selectThumbnail = (e) => {
+      const files = e.target.files;
+      for (let i = 0; i < files.length; i++) {
+        createGig.value.banner.push(files[i]);
+      }
+      document.querySelector('#bannerInput').value='';
+      console.log(createGig.value);
+
+      bannersBase64.value=[];
+      createGig.value.banner.forEach(img => {
+          encodeImageFileAsURL(img);
+      });
+    };
+    const encodeImageFileAsURL =file => {    
+        const reader = new FileReader();  
+        reader.onloadend = function() {  
+            bannersBase64.value.push(reader.result);  
+        }  
+        reader.readAsDataURL(file);  
+    }
+
+    const removeImage = index => {
+        bannersBase64.value.splice(index, 1);
+        createGig.value.banner.splice(index, 1);
+    }
+
+    return{
+        data,
+        gigCreation,
+        selectThumbnail,
+        removeImage,
+        encodeImageFileAsURL,
+        createGig,
+        bannersBase64,
+        convertFileToBase64,
+
+    }
+}
