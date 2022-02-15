39ad3346398889e1092c9a805d4fd69180138414
Asjad-Ali <92371450+Asjad-Ali@users.noreply.github.com>
2022-02-15 16:48:36 +0500

    router file

--- src/router/index.js
+++ src/router/index.js
@@ -6,17 +6,14 @@ import BecomeSeller from '../views/Become-Seller.vue'
 import Profile from '../views/Profile.vue'
 import ServiceDetail from '../views/ServiceDetail.vue'
 
-import Buyer from '../views/Buyer.vue'
 import CreateJob from '../components/buyer/CreateJob.vue'
 import Jobs from '../components/buyer/AllJobs.vue'
 import AllOrders from '../components/buyer/AllOrders.vue'
 
-import Seller from '@/views/Seller.vue'
 import CreateService from '@/components/seller/CreateService.vue'
 import ManageService from '@/components/seller/ManageService.vue'
 import UpdateService from '@/components/seller/UpdateService.vue'
 import BuyerRequests from '@/components/seller/BuyerRequestSection.vue'
-
 
 
 const routes = [
@@ -25,41 +22,11 @@ const routes = [
     name: 'Home',
     component: Home,
   },
-
   {
     path: '/login',
     name: 'Login',
     component: Login
   },
-  {
-    path: '/seller',
-    name: 'Seller',
-    component: Seller,
-    children:[
-      {
-        path: 'create_service',
-        name: 'CreateService',
-        component: CreateService
-      },
-      {
-        path: 'manage_service',
-        name: 'ManageService',
-        component: ManageService
-      },
-      {
-        path: 'update_service/:id',
-        name: 'UpdateService',
-        component: UpdateService
-      },
-      {
-        path: 'buyer_requests',
-        name: 'BuyerRequests',
-        component: BuyerRequests
-      },
-
-    ]
-  },
- 
   {
     path: '/register',
     name: 'Register',
@@ -69,40 +36,51 @@ const routes = [
     path: '/become_seller',
     name: 'BecomeSeller',
     component: BecomeSeller,
-    
   },
   {
     path: '/Profile',
     name: 'Profile',
     component: Profile,
-  
   },
 
   {
-    path: '/buyer',
-    name: 'Buyer',
-    component: Buyer,
-    children:[
-      {
-        path: 'create_job',
-        name: 'CreateJob',
-        component: CreateJob,
-      },
-      {
-        path: 'jobs',
-        name: 'Jobs',
-        component: Jobs,
-      },
-      {
-        path: 'manage_order',
-        name: 'ManageOrder',
-        component: AllOrders,
-      },
-    ]
+    path: '/seller/create_service',
+    name: 'CreateService',
+    component: CreateService
+  },
+  {
+    path: '/seller/manage_service',
+    name: 'ManageService',
+    component: ManageService
+  },
+  {
+    path: '/seller/update_service/:id',
+    name: 'UpdateService',
+    component: UpdateService
+  },
+  {
+    path: '/seller/buyer_requests',
+    name: 'BuyerRequests',
+    component: BuyerRequests
   },
 
+ 
 
-  
+  {
+    path: '/buyer/create_job',
+    name: 'CreateJob',
+    component: CreateJob,
+  },
+  {
+    path: '/buyer/jobs',
+    name: 'Jobs',
+    component: Jobs,
+  },
+  {
+    path: '/buyer/manage_order',
+    name: 'ManageOrder',
+    component: AllOrders,
+  },
 
   {
     path: '/gigs',
