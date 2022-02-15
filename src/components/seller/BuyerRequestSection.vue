95dc553156b78f6963834f6d6d9aed4e40f19629
Muhammad Arslan <arslanarif8088@ucp.edu.pk>
2022-02-15 11:38:44 +0500

    buyerRequestSection updated

--- src/components/seller/BuyerRequestSection.vue
+++ src/components/seller/BuyerRequestSection.vue
@@ -1,48 +1,52 @@
 <template>
   <div class="container pt-3">
-    <div class="row">
-      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-        <div
-          v-for="request in requests"
-          :key="request.id"
-        >
-        <!-- {{ request.id }} -->
-          <div class="card">
-            <h5 class="card-header bg-white">
-              <div class="d-flex align-items-end">
-                  <img
-                    class="img-fluid bg-info rounded-circle mr-2" style="width:55px;height:55px;"
-                    :src="
-                      'https://api.dex.proelean.com/' + request.user.image
-                    "
-                    alt=""
-                  />
-                  <div class="row d-flex flex-column">
-                    <div class="col">
-
-                      <span class="seller-name">
-                        <p> {{ request.user.username }} </p>
-                      </span>
-                      <div style="font-size:12px;"> {{ request.created_at }} </div> 
-                    </div>
-                  </div>
-                </div>
-              </h5>
-            <div class="card-body">
-              <p class="card-text bg-light">
-                {{ request.description }}
-              </p>
-              <div class='d-flex justify-content-start flex-column my-1'>
-                <div class='mb-2'> Duration: {{ request.delivery_time }} </div>
-                <div class='mb-2'> budget: {{ request.budget }}$ </div>
-                <div class='mb-2'> Document: No Attachment </div>
-              </div>
-              <div class='d-flex justify-content-end'>
-                <a href="#" class="btn btn-dark">{{ request.total_offers }} Offers Sent</a>
-                <a href="#" class="btn btn-primary ml-2" :style="{'pointer-events': request.is_applied ? 'none':''}">{{ request.is_applied ? 'Applied' : 'Send Offer'}}</a>
-                <a href="#" class="btn btn-success ml-2">Cancel Offer</a>
+    <div v-for="request in requests" :key="request.id">
+      <div class="card">
+        <h5 class="card-header bg-white">
+          <div class="row">
+            <div class="col-lg-6 d-flex justify-content-start">
+              <img
+                class="img-fluid bg-info rounded-circle mr-3"
+                style="width: 60px; height: 55px"
+                :src="'https://api.dex.proelean.com/' + request.user.image"
+                alt=""
+              />
+              <div class="d-flex flex-column">
+                <span class="seller-name">
+                  <p>{{ request.user.username }}</p>
+                </span>
+                <div style="font-size: 12px">{{ request.created_at }}</div>
               </div>
             </div>
+            <div class="col-lg-6 col-md-12 d-flex justify-content-end">
+              <div class="d-flex flex-column my-1">
+                <span class="mb-2" style="font-size: 14px"
+                  >Duration: {{ request.delivery_time }}</span
+                >
+                <span class="mb-2" style="font-size: 14px"
+                  >budget: {{ request.budget }}$</span
+                >
+              </div>
+            </div>
+          </div>
+        </h5>
+        <div class="card-body">
+          <p class="card-text bg-light">
+            {{ request.description }}
+          </p>
+          <div class="d-flex justify-content-start flex-column my-1">
+            <div class="mb-2">Document: No Attachment</div>
+          </div>
+          <div class="d-flex justify-content-end">
+            <btn class="btn btn-dark"
+              >{{ request.total_offers }} Offers Sent</btn
+            >
+            <btn
+              class="btn btn-primary ml-2"
+              :style="{ 'pointer-events': request.is_applied ? 'none' : '' }"
+              >{{ request.is_applied ? "Applied" : "Send Offer" }}</btn
+            >
+            <btn class="btn btn-success ml-2">Cancel Offer</btn>
           </div>
         </div>
       </div>
@@ -56,7 +60,7 @@ import store from "../../store";
 export default {
   setup() {
     return {
-      requests: computed(() => store.getters.getBuyerRequests)
+      requests: computed(() => store.getters.getBuyerRequests),
     };
   },
 };
