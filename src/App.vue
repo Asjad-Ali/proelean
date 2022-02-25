488a97aaae6dbf9a11b2100cb6ae61d662e8ddb0
Muhammad Arslan <arslanarif8088@ucp.edu.pk>
2022-02-25 18:50:17 +0500

    layout updated

--- src/App.vue
+++ src/App.vue
@@ -2,57 +2,52 @@
   <div>
     <TopNav />
     <div class="d-none d-lg-block">
-
-    <BottomNav v-if="isAuthenticated" />
+      <BottomNav v-if="isAuthenticated" />
     </div>
-    <router-view/>
+    <router-view />
     <Footer />
-<<<<<<< HEAD
-=======
     <div class="d-lg-none">
-    <MobileBottomNav />
-</div>
->>>>>>> 98d65f2909710927503c0079b414e25c50519244
+      <MobileBottomNav/>
+    </div>
   </div>
 </template>
 
 
 <script>
-import TopNav from './components/layouts/TopNav.vue';
-import BottomNav from './components/layouts/BottomNav.vue';
-import Footer from './components/layouts/Footer.vue';
+import TopNav from "./components/layouts/TopNav.vue";
+import BottomNav from "./components/layouts/BottomNav.vue";
+import Footer from "./components/layouts/Footer.vue";
+import MobileBottomNav from "./components/layouts/MobileBottomNav.vue";
+
 export default {
   name: "App",
-  components:{
+  components: {
     TopNav,
     BottomNav,
-    Footer
+    Footer,
+    MobileBottomNav
   },
   setup() {
-    
     return {
       isAuthenticated: localStorage.getItem("PROELEAN_TOKEN"),
-    }
-
-
-  }
-}
+    };
+  },
+};
 </script>
 
 <style lang="scss">
-.cursor-pointer{
+.cursor-pointer {
   cursor: pointer;
 }
 .gig-img-outer {
-    height: 200px;
-    width: 100%;
+  height: 200px;
+  width: 100%;
 }
 
-.img-full{
+.img-full {
   height: 100%;
   width: 100%;
   overflow: hidden;
   object-fit: cover;
 }
-
 </style>
