9780a1e3fd22b6a001647588638f9e666c907a03
unknown <Asif.ali14280@gmail.com>
2022-02-28 19:08:23 +0500

    ui chnages

--- src/components/seller/ManageService.vue
+++ src/components/seller/ManageService.vue
@@ -151,12 +151,12 @@
             <div v-if="services.length" class="table-responsive box-table mt-3 bg-white">
               <table class="table table-bordered">
                 <thead>
-                  <tr>
-                    <th>THUMBNAIL</th>
-                    <th>SERVICE DESCRIPTION</th>
-                    <th>DURATION</th>
-                    <th>BUDGET</th>
-                    <th>ACTION</th>
+                  <tr >
+                    <th>Thumbnail</th>
+                    <th>Service Description</th>
+                    <th>Duration</th>
+                    <th>Budget</th>
+                    <th>Action</th>
                   </tr>
                 </thead>
                 <tbody>
@@ -167,7 +167,7 @@
                     <td>
                       <img
                         style="height: 120px; width: 220px"
-                        class="img-thumbnail"
+                        class="img-thumbnail border-0"
                         :src="
                           service.service_media.length &&
                           service.service_media[0].media
