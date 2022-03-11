5f30622f16606347fb600493a3c3b3aaa44ff379
unknown <Asif.ali14280@gmail.com>
2022-03-11 15:12:08 +0500

    extra files images deleted

--- src/components/Seller/Dashboard/AnalyticBoxes.vue
+++ src/components/Seller/Dashboard/AnalyticBoxes.vue
@@ -4,7 +4,7 @@
       <div class="app-card app-card-stat shadow-sm h-100">
         <div class="app-card-body p-3 p-lg-4">
           <h4 class="stats-type mb-1">Balance</h4>
-          <div class="stats-figure">{{user.availabe_balance}}</div>
+          <div class="stats-figure">€ {{user.availabe_balance}}</div>
         </div>
         <!--//app-card-body-->
         <a class="app-card-link-mask" href="#"></a>
--- src/views/Seller/Notifications.vue
+++ src/views/Seller/Notifications.vue
@@ -1,115 +1,141 @@
 <template>
-  
-       
-            <div class="row d-flex justify-content-center col-12 m-0 p-0">
-               <div class="col-md-12">
-                  <h1 class="app-page-title">Notifications</h1>
-                  <div class="border-bottom my-3"></div>
-                  <div class="box shadow-sm rounded bg-white mb-3">
-                     <div class="box-title border-bottom p-3">
-                        <h6 class="m-0">All Notifications</h6>
-                     </div>
-                     <div v-if="earlierNotification.length > 0">
-                        <div  class="box-body p-0" v-for="notification in earlierNotification" :key="notification.index">
-                        <div class="p-3 d-flex align-items-center bg-light border-bottom osahan-post-header">
-                              <div class="dropdown-list-image mr-3">
-                                 <img class="rounded-circle" src="/assets/images/user/s4.png" alt="">
-                              </div>
-                              <div class="font-weight-bold mr-3">
-                                 <div class="text-truncate">{{ notification.name }}</div>
-                                 <div class=" text-muted ">{{ notification.body }}</div>
-                              </div>
-                              <span class="ml-auto my-auto">
-                                 <div class="d-flex">
-                                 <div class="text-right text-muted ml-2">{{ $filters.timeAgo( notification.created_at ) }}</div>
-                                    <div>
-                                       <i class="mdi text-danger cursor-pointer mdi-delete fa-lg p-1 m-2"
-                                          data-toggle="modal"
-                                          data-target="#exampleModalCenter"></i>
-                                    </div>
-                                 <div
-                                    class="modal fade"
-                                    id="exampleModalCenter"
-                                    tabindex="-1"
-                                    role="dialog"
-                                    aria-labelledby="exampleModalCenterTitle"
-                                    aria-hidden="true"
-                                 >
-                                    <div
-                                    class="modal-dialog modal-dialog-centered"
-                                    role="document"
-                                    >
-                                    <div class="modal-content">
-                                       <div
-                                          class="modal-header d-flex justify-content-center"
-                                       >
-                                          <h5
-                                          class="modal-title"
-                                          id="exampleModalLongTitle"
-                                          >
-                                          Delete Notification
-                                          </h5>
-                                       </div>
-                                       <div class="modal-body text-center">
-                                          Are you sure you want to delete the service?
-                                       </div>
-                                       <div
-                                          class="modal-footer d-flex justify-content-center"
-                                       >
-                                          <button
-                                          type="button"
-                                          class="btn btn-secondary"
-                                          data-dismiss="modal"
-                                          >
-                                          No
-                                          </button>
-                                          <button
-                                          type="button"
-                                          class="btn btn-danger"
-                                          data-dismiss="modal"
-                                          @click="deleteNotification"
-                                          >
-                                          Yes
-                                          </button>
-                                       </div>
-                                    </div>
-                                    </div>
-                                 </div>
-                                 </div>
-                              </span>
-                           </div>
-                        </div>
-                     </div>
-
-                     <div v-else>
-                        <h4>Not Any Notification</h4>
-                     </div>
-                     
+  <div class="row d-flex justify-content-center col-12 m-0 p-0">
+    <div class="col-md-12">
+      <h1 class="app-page-title">Notifications</h1>
+      <div class="border-bottom my-3"></div>
+      <div class="box shadow-sm rounded bg-white mb-3">
+        <div class="box-title border-bottom p-3">
+          <h6 class="m-0">All Notifications</h6>
+        </div>
+        <div v-if="earlierNotification.length > 0">
+          <div
+            class="box-body p-0"
+            v-for="notification in earlierNotification"
+            :key="notification.index"
+          >
+            <div
+              class="
+                p-3
+                d-flex
+                align-items-center
+                bg-light
+                border-bottom
+                osahan-post-header
+              "
+            >
+              <div class="dropdown-list-image mr-3">
+                <img
+                  class="rounded-circle"
+                  src="/assets/images/user/s4.png"
+                  alt=""
+                />
+              </div>
+              <div class="font-weight-bold mr-3">
+                <div class="text-truncate">{{ notification.name }}</div>
+                <div class="text-muted">{{ notification.body }}</div>
+              </div>
+              <span class="ml-auto my-auto">
+                <div class="d-flex">
+                  <div class="text-right text-muted ml-2">
+                    {{ $filters.timeAgo(notification.created_at) }}
                   </div>
-
-               </div>
+                  <div>
+                    <i
+                      class="
+                        mdi
+                        text-danger
+                        cursor-pointer
+                        mdi-delete
+                        fa-lg
+                        p-1
+                        m-2
+                      "
+                      data-toggle="modal"
+                      data-target="#exampleModalCenter"
+                    ></i>
+                  </div>
+                  <div
+                    class="modal fade"
+                    id="exampleModalCenter"
+                    tabindex="-1"
+                    role="dialog"
+                    aria-labelledby="exampleModalCenterTitle"
+                    aria-hidden="true"
+                  >
+                    <div
+                      class="modal-dialog modal-dialog-centered"
+                      role="document"
+                    >
+                      <div class="modal-content">
+                        <div class="modal-header d-flex justify-content-center">
+                          <h5 class="modal-title" id="exampleModalLongTitle">
+                            Delete Notification
+                          </h5>
+                        </div>
+                        <div class="modal-body text-center">
+                          Are you sure you want to delete the service?
+                        </div>
+                        <div class="modal-footer d-flex justify-content-center">
+                          <button
+                            type="button"
+                            class="btn btn-secondary"
+                            data-dismiss="modal"
+                          >
+                            No
+                          </button>
+                          <button
+                            type="button"
+                            class="btn btn-danger"
+                            data-dismiss="modal"
+                            @click="deleteNotification"
+                          >
+                            Yes
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </span>
             </div>
- 
+          </div>
+        </div>
+
+   <div class="app-card-body " v-else>
+   <div class="table-responsive h50vh flex-width-center">
+      <div class="m-5 text-center">
+         <h5>No notifications found</h5>
+         <figure class="py-3 m-0">
+            <img
+               src="/assets/seller/images/svg-icons/notify.svg"
+               alt="notfound"
+               width="150"
+               />
+         </figure>
+      </div>
+   </div>
+</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import { computed } from "vue"
-import { useStore } from 'vuex'
+import { computed } from "vue";
+import { useStore } from "vuex";
 export default {
-   setup(){
-      const store = useStore()
-      function deleteNotification () {
-         console.log("Deleted Successfully")
-      }
-      return{
-         deleteNotification,
-         earlierNotification: computed( () => store.getters.getAllNotifications),
-      }
-   }
-
-}
+  setup() {
+    const store = useStore();
+    function deleteNotification() {
+      console.log("Deleted Successfully");
+    }
+    return {
+      deleteNotification,
+      earlierNotification: computed(() => store.getters.getAllNotifications),
+    };
+  },
+};
 </script>
 
 <style>
-
 </style>
