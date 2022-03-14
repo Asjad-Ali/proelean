3bb48286e27792345368aa3bce42b8679a0f2046
AsifAli13280 <asifali@horizam.com>
2022-03-14 19:39:03 +0500

    chat updated

--- src/router/index.js
+++ src/router/index.js
@@ -115,7 +115,7 @@ const routes = [
     component: Purchase,
   },
   {
-    path: '/chat/:id',
+    path: '/chat/:id?',
     name: 'Chat',
     component: () => import(/* webpackChunkName: "chat" */'@/views/Chat.vue'),
   },
