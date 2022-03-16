caf253e7090410e91b82ce4ddf2f50a968213509
unknown <Asif.ali14280@gmail.com>
2022-03-16 19:23:46 +0500

    order-detail page created

--- src/components/Seller/Dashboard/CountDown.vue
+++ src/components/Seller/Dashboard/CountDown.vue
@@ -6,131 +6,55 @@
       shadow-sm
       mb-4
       border-left-decoration
+      pb-3
     "
   >
-    <div class="row">
-      <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-        <div id="main">
-          <div class="content">
-            
-            <!-- /.Text Div -->
-            <div class="counter">
-              <h3>Estimated Time To Deliver</h3>
-              <div id="countdown"></div>
-              <!-- /#Countdown Div -->
-            </div>
-            <!-- /.Counter Div -->
-          </div>
-          <!-- /.Content Div -->
+    <div class="row d-flex justify-content-center text-center">
+      <div class="col-lg-3 col-md-12 col-12">
+        <h4>Days</h4>
+        <div class="covid-content">
+          <h1>00</h1>
         </div>
-        <!-- /#Main Div -->
       </div>
-      <!-- /.Columns Div -->
+      <div class="col-lg-3 col-md-12 col-12">
+        <h4>Hours</h4>
+        <div class="covid-content">
+          <h1>20</h1>
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-12 col-12">
+        <h4>Minutes</h4>
+        <div class="covid-content">
+          <h1>30</h1>
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-12 col-12">
+        <h4>Seconds</h4>
+        <div class="covid-content">
+          <h1>22</h1>
+        </div>
+      </div>
     </div>
-    <!-- /.Row Div -->
   </div>
 </template>
 
 <script>
-export default {
-setup(){
-  // set the date we're counting down to
-var target_date = new Date('Jan, 31, 2014').getTime();
- 
-// variables for time units
-var days, hours, minutes, seconds, current_date, seconds_left ;
- 
-// get tag element
-var countdown = document.getElementById('countdown');
- 
-// update the tag with id "countdown" every 1 second
-setInterval(function () {
- 
-    // find the amount of "seconds" between now and target
-    var current_date = new Date().getTime();
-    var seconds_left = (target_date - current_date) / 1000;
- 
-    // do some time calculations
-    days = parseInt(seconds_left / 86400);
-    seconds_left = seconds_left % 86400;
-     
-    hours = parseInt(seconds_left / 3600);
-    seconds_left = seconds_left % 3600;
-     
-    minutes = parseInt(seconds_left / 60);
-    seconds = parseInt(seconds_left % 60);
-     
-    // format countdown string + set tag value
-    countdown.innerHTML = '<span class="days">' + days +  ' <b>Days</b></span> <span class="hours">' + hours + ' <b>Hours</b></span> <span class="minutes">'
-    + minutes + ' <b>Minutes</b></span> <span class="seconds">' + seconds + ' <b>Seconds</b></span>';  
- 
-}, 1000);
-return{
-  target_date,
-  days,
-  hours,
-  minutes,
-  seconds,
-  countdown,
-  current_date,
-  seconds_left
-}
-
-}
-};
-
+export default {};
 </script>
 
 <style scoped>
-
-
- 
-.text {
-  border-bottom: 1px solid #262626;
-  margin-top: 40px;
-  padding-bottom: 40px;
-  text-align: center;
+.covid-content {
+    background: #14222B;
+    color: #2DBCB2;
+    padding: 16px;
+    border-radius: 10px;
 }
-
-.text h2 {
-  color: #e5e5e5;
-  font-size: 30px;
-  font-style: normal;
-  font-variant: normal;
-  font-weight: lighter;
-  letter-spacing: 2px;
+  h4 {
+    font-weight: 600;
+    padding-bottom: 10px;
+    padding-top: 10px;
 }
-
-.counter {
-  background: #2c2c2c;
-  -moz-box-shadow: inset 0 0 5px #000000;
-  -webkit-box-shadow: inset 0 0 5px #000000;
-  box-shadow: inset 0 0 5px #000000;
-  min-height: 150px;
-  text-align: center;
-}
-
-.counter h3 {
-  color: #e5e5e5;
-  font-size: 14px;
-  font-style: normal;
-  font-variant: normal;
-  font-weight: lighter;
-  letter-spacing: 1px;
-  padding-top: 20px;
-  margin-bottom: 30px;
-}
-
-#countdown {
-  color: #ffffff;
-}
-
-#countdown span {
-  color: #e5e5e5;
-  font-size: 26px;
-  font-weight: normal;
-  margin-left: 20px;
-  margin-right: 20px;
-  text-align: center;
+h1{
+  color: #2DBCB2;
 }
 </style>
