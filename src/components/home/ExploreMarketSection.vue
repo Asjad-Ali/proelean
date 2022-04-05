2eeef5b3f32093c6896b12f5347ca88b395777f2
AsifAli13280 <asifali@horizam.com>
2022-04-05 10:13:11 +0500

    explore market section updated

--- src/components/home/ExploreMarketSection.vue
+++ src/components/home/ExploreMarketSection.vue
@@ -12,13 +12,8 @@
         <div class="card shadow-none">
           <router-link :to="{name:'Gigs', params:{slug: category.slug}}">
           <img :src="`${category.banner.includes('https') || category.banner.includes('http') ? category.banner : imgURL+'/'+category.banner}`" class="card-img-top" alt="Card Image">
-<<<<<<< HEAD
-          <div class="card-body d-flex flex-column margn">
-            <h5 class="card-title text-center">{{ category.title }}</h5>
-=======
           <div class="card-body d-flex flex-column margn text-center">
             <h5 class="card-title">{{ category.title }}</h5>
->>>>>>> deb80317e6457cfa38fb94d60729579af86ba0ea
           </div>
           </router-link>
         </div>
